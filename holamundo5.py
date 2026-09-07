a4455115875e17d46763fdc624ae024836e4218d
JHOEL <76185723@institutourusayhua.edu.pe>
2026-09-07 11:35:04 -0500

    Add files via upload

---
+++ holamundo5.py
@@ -0,0 +1,20 @@
+# Cómo asignar un nuevo valor a una variable ya existente
+var = 1
+print("variable con un valor primario: ", var)
+var = var + 1
+print("variable con un nuevo valor asignado: ", var)
+var = 100
+var = 200 + 300
+print(var)
+#  LAB   Variables
+juan_david = 3
+madison = 5
+alex = 6
+print("juan_david tiene ", juan_david,"manzanas -->", "madison tiene ", madison,"manzanas -->", "alex tiene ", alex,"manzanas")
+total_manzanas = juan_david + madison + alex
+print("Total de manzanas es: ", total_manzanas)
+# Operadores Abreviados
+x = 3
+# x = x * 2
+x *= 2
+print("El resultado es: ",x)
