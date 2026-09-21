170f9b25a9b766cea9c5d393c7367faba78c1641
JHOEL <76185723@institutourusayhua.edu.pe>
2026-09-21 11:32:49 -0500

    Add Operadorescadena.py for string operations

---
+++ Operadorescadena.py
@@ -0,0 +1,17 @@
+print("cadena 1"+ "cadena 2")
+nom1 = "Juan David"
+nom2 = "Alexander"
+print(nom1 + " y " + nom2 + ": son compañeros")
+
+nombre = input("¿Me puedes dar tu nombre por favor? ")
+apellido = input("¿Me puedes dar tu apellido por favor? ")
+print("Gracias. ")
+print("\nTu nombre es " + nombre + " " + apellido + ".")
+
+print("james" * 3)
+print(3 * "an")
+print(5 * "2")
+
+print("+" + 100 * "-" + "+")
+print(("|" + " " * 100 + "|\n") * 30, end="")
+print("+" + 100 * "-" + "+")
