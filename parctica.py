7aa0681f53971c2219a14bc46f9eda837598c439
JHOEL <76185723@institutourusayhua.edu.pe>
2026-09-25 12:03:28 -0500

    Add basic arithmetic operations in parctica.py

---
+++ parctica.py
@@ -0,0 +1,23 @@
+# nombre = input("¿Me puedes dar tu nombre por favor? ")
+# apellido = input("¿Me puedes dar tu apellido por favor? ")
+# print("Gracias. " +"\nTu nombre es " + nombre + " " + apellido + ".")
+numbre1 = int(50.56)
+numbre2 = float(40)
+number3 = str(25.68)
+print (str(numbre1)," ",str(numbre2)," ",str(number3))
+
+# ingresa un valor flotante para la variable a aquí
+numero1 = float(input("Ingrese el primer numero flotante:"))
+# ingresa un valor flotante para la variable b aquí
+numero2 = float(input("Ingrese el segundo numero flotante:"))
+suma = numero1 + numero2
+resta = numero1 + numero2
+multiplicacion = numero1 + numero2
+division = numero1 + numero2
+
+# mostrar el resultado de la suma aquí
+# mostrar el resultado de la resta aquí
+# mostrar el resultado de la multiplicación aquí
+# mostrar el resultado de la división aquí
+
+print (str(suma)," ",str(resta)," ",str(multiplicacion)," ",str(division))
